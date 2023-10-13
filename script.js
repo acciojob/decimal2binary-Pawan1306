b397c8c018daa0945c5addbf5f3f0739f042f506
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-13 17:06:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function decimalToBinary(num) {
   if(num==0){
 	  return 0;
   }
-  let binaryNumber= " ";
+  let binaryNumber= "";
 	while(num>0){
 		binaryNumber = (num % 2) + binaryNumber;
 		num = Math.floor(num/2)
